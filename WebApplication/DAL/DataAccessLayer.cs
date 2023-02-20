a804566a2ecaf59077276582428dd7ce3e9c67fd
Antonio Juko <an4037ju-s@student.lu.se>
2023-02-20 14:44:02 +0000

    Delete metod

--- WebApplication/DAL/DataAccessLayer.cs
+++ WebApplication/DAL/DataAccessLayer.cs
@@ -56,6 +56,56 @@ public static Employee GetEmployeeByNo(string no)
         }
         return employee;
     }
+        public static int TableCount()
+        {
+            int count = 0;
+            using (SqlConnection connection = ConnectionHandler.GetSqlServerConnection())
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+                var command = new SqlCommand(query, connection);
+                count = (int)command.ExecuteScalar();
+            }
+            return count;
+        }
+
+        public static void CreateEmployee(Employee employee)
+        {
+            using (SqlConnection connection = ConnectionHandler.GetSqlServerConnection())
+            {
+                connection.Open();
+
+                string query = "INSERT INTO [CRONUS Sverige AB$Employee] ([No_], [First Name], [Last Name], [Job Title], [City]) VALUES (@No_, @FirstName, @LastName, @JobTitle, @City)";
+                var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@No_", employee.No);
+                command.Parameters.AddWithValue("@FirstName", employee.FirstName);
+                command.Parameters.AddWithValue("@LastName", employee.LastName);
+                command.Parameters.AddWithValue("@JobTitle", employee.JobTitle);
+                command.Parameters.AddWithValue("@City", employee.City);
+
+                command.ExecuteNonQuery();
+            }
+
+        }
+        public static void DeleteEmployee(string no)
+        {
+            using (SqlConnection connection = ConnectionHandler.GetSqlServerConnection())
+            {
+                connection.Open();
+
+                string query = "DELETE FROM [CRONUS Sverige AB$Employee] WHERE [No_] = @No_";
+                var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@No_", no);
+
+
+                command.ExecuteNonQuery();
+
+
+
+
+
+            }
+        }
     }
 }
 
